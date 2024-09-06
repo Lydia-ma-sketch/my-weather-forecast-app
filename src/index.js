4415b0c03e144d7eb649e3197f611f35d49afe74
Lydia-ma-sketch <kk.lydia@gmail.com>
2024-09-06 11:27:31 +0630

    Added axios link

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ function handleSearch(event) {
   let h1 = document.querySelector("h1");
   h1.innerHTML = city.value;
 }
-
+// The event will be triggered by clicking the button or pressing enter
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
