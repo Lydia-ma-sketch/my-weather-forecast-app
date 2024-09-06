e44fb10fd86655d8bb4bb7409c9e563f122be8a2
Lydia-ma-sketch <kk.lydia@gmail.com>
2024-09-06 11:08:21 +0630

    Added js for search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = city.value;
+}
 
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearch);
